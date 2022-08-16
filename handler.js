0957ea187c8e0806bb894494f4aa3081b7a4b485
somnathChaturvedi <somnath353@gmail.com>
2022-08-16 21:46:01 +0530

    promise await

--- handler.js
+++ handler.js
@@ -1,41 +1,43 @@
-const tf = require('@tensorflow/tfjs')
-const mobilenet = require('@tensorflow-models/mobilenet');
-global.fetch = require('node-fetch');
+import tf from "@tensorflow/tfjs";
+import mobilenet from "@tensorflow-models/mobilenet";
+import fetch from "node-fetch";
+globalThis.fetch = fetch;
+import image from "get-image-data";
 
-const image = require('get-image-data');
-let response;
+export const lambdaHandler = async (event, context, callback) => {
+    var imgurl = JSON.parse(event.body);
+    let url = imgurl.url;
+    const classification = await classify(url);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        classification
+    })
+    };
+    callback(null, response);
+};
 
-module.exports.getname = async (event, context, callback) => {
-  try {
-    let url = event.url;
-    response = whatIsThis(url);
-    function whatIsThis(url) {
-      return new Promise((resolve, reject) => {
-        image(url, async (err, image) => {
-          if (err) {
-            reject(err);
-          } else {
-            const channelCount = 3;
-            const pixelCount = image.width * image.height;
-            const vals = new Int32Array(pixelCount * channelCount);
-            let pixels = image.data;
-            for (let i = 0; i < pixelCount; i++) {
-              for (let k = 0; k < channelCount; k++) {
-                vals[i * channelCount + k] = pixels[i * 4 + k];
-              }
-            }
-            const outputShape = [image.height, image.width, channelCount];
-            const input = tf.tensor3d(vals, outputShape, "int32");
-            const model = await mobilenet.load();
-            let temp = await model.classify(input);
-            resolve(temp);
+function classify(url) {
+  return new Promise((resolve, reject) => {
+    image(url, async (err, image) => {
+      if (err) {
+        reject(err);
+      } else {
+        const channelCount = 3;
+        const pixelCount = image.width * image.height;
+        const vals = new Int32Array(pixelCount * channelCount);
+        let pixels = image.data;
+        for (let i = 0; i < pixelCount; i++) {
+          for (let k = 0; k < channelCount; k++) {
+            vals[i * channelCount + k] = pixels[i * 4 + k];
           }
-        });
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-  return response;
-  };
-
+        }
+        const outputShape = [image.height, image.width, channelCount];
+        const input = tf.tensor3d(vals, outputShape, "int32");
+        const model = await mobilenet.load();
+        let temp = await model.classify(input);
+        resolve(temp);
+      }
+    });
+  });
+}
